0a90b14bdb538a732de1ce411b710d29545ab0c6
jularbruno <brunojular00@gmail.com>
2019-03-27 21:28:05 -0300

    This is like the 18th time I try to do this

--- Config/projectInsider/views.py
+++ Config/projectInsider/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
+ 
+
+def inicio(request):
+
+	context = {}
+
+	return render(request, "home.html",context )
