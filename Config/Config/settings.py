a4838adb185b53e314c98526a3532280251b8ae4
JularBruno <brunojular00@gmail.com>
2019-06-05 08:44:14 +0000

    wat

--- Config/Config/settings.py
+++ Config/Config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/brunojular/Desktop/ProjectInsider/Config/Config/static/'
+STATIC_ROOT = '/home/BrunoJular/projectInsider/Config/projectInsider/static/'
 
 
 
